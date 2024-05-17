e7ce823c2eb62db4358f7d5507790441700192f1
sajid_shaikh <sajid.shaikh@brainvire.com>
2024-05-17 11:40:52 +0530

    in this branch i have implement getxNavigation Basics

--- lib/splash/ui/splash_page.dart
+++ lib/splash/ui/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_start2may/splash/controller/splash_controller.dart';
 import '../../../utils/exports.dart';
 
 class Splash extends StatefulWidget {
@@ -8,7 +9,10 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+
   late Future<void> _navigationFuture;
+
+  final SplashController controller = Get.put(SplashController());
 
   @override
   void initState() {
@@ -18,17 +22,35 @@ class _SplashState extends State<Splash> {
 
   Future<void> _navigateAfterDelay() async {
     await Future.delayed(const Duration(seconds: 3));
-
     Get.off(const LoginScreen());
-
     //context.goNamed(AppRoutes.rootLoginRoute.name);
     //GoRouter.of(context).goNamed(AppRoutes.rootLoginRoute.name);
   }
 
   @override
   Widget build(BuildContext context) {
+    print("buildmethod");
     return Scaffold(
-      body: Center(child: Image.asset("assets/images/Splash_logo.png")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Image.asset("assets/images/Splash_logo.png")),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Obx(() {
+                return Text(
+                  controller.counter.string, style: TextStyle(fontSize: 30));
+              })
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("add"),
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          controller.increamentCounter();
+        },
+      ),
     );
   }
 
